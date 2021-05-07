bcdabe682c775ca8050b7d716f8e4f56257fc357
Nico Mendoza <blankartwp@gmail.com>
2021-05-07 20:51:28 +0800

    fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,10 +13,10 @@ require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
 
 tests_add_filter( 'muplugins_loaded', function() {
     if ( getenv( 'MYSQL_GITHUB_ACTION' ) ) {
-        // Use the build plugin for github action.
-        require( dirname( __DIR__ ) . '/build/stackable/plugin.php' );
+        // Use the build folder to access Stackable.
+        require dirname( __DIR__ ) . '/build/stackable/plugin.php';
     } else {
-        // Otherwise, use the root plugin.php
+        // Otherwise, use the dev plugin.php file.
         require dirname( __DIR__ ) . '/plugin.php';
     }
 } );
