6b0f04ea447064cd2c59bb1b79eeb15b05bee3e1
Nico Mendoza <blankartwp@gmail.com>
2021-05-07 16:30:08 +0800

    refactor

---
+++ tests/pluginTest.php
@@ -0,0 +1,14 @@
+<?php
+class PluginTest extends WP_UnitTestCase {
+
+	/** @test **/
+	function test_wordpress_and_stackable_are_loaded() {
+		$this->assertTrue( function_exists( 'do_action' ) );
+		$this->assertTrue( function_exists( 'add_filter' ) );
+		$this->assertTrue( function_exists( 'apply_filters' ) );
+		$this->assertTrue( function_exists( 'stackable_php_requirement_activation_check' ) );
+		$this->assertTrue( function_exists( 'stackable_version_upgrade_check' ) );
+		$this->assertTrue( function_exists( 'stackable_notice_gutenberg_plugin_activated' ) );
+		$this->assertTrue( function_exists( 'stackable_notice_gutenberg_plugin_ignore' ) );
+	}
+}
